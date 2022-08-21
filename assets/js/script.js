8c4d504015b7e12a63ed72fc3906d574267914af
AndrewFaugno <andrewfaugno825@gmail.com>
2022-08-21 16:38:07 -0400

    created prompts and password character generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,63 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // define vars
+  let upper;
+  let lower;
+  let numbers;
+  let special;
+
+  // get password length, re-prompt if value is not a int between 8-128
+  while (true) {
+    if (length < 8 || length > 128) {
+      length = prompt('Please enter password length\n(8 - 128)')
+    } else {
+      break;
+    }
+  }
+
+  // prompt for uppercase letters
+  if (confirm('Would you like uppercase letters in your password?')) {
+    upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  } else {
+    upper = '';
+  }
+
+  if (confirm('Would you like lowercase letters in your password?')) {
+    lower = 'abcdefghijklmnopqrstuvwxyz';
+  } else {
+    lower = '';
+  }
+
+  if (confirm('Would you like numbers in your password?')) {
+    numbers = '0123456789';
+  } else {
+    numbers = ''
+  }
+
+  if (confirm('Would you like special characters in your password?')) {
+    special = "~`!@#$%^&*_-+=|',.?/";
+  } else {
+    special = '';
+  }
+
+  let passwordChars = upper.concat(lower, numbers, special);
+
+  var password = generatePassword(length, passwordChars);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function generatePassword(legnth, passwordChars) {
+  let password = '';
+  for (let i = 0; i < length; i++) {
+    let randomNumber = Math.floor(Math.random() * passwordChars.length);
+    password += passwordChars.substring(randomNumber, randomNumber + 1);
+  }
+  return password;
 }
 
 // Add event listener to generate button
